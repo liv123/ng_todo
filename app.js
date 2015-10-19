128cb81a0d965a96199da4c629a4909a184796e8
liv123 <olivia.e@gmail.com>
2015-10-18 19:44:58 -0700

    Angular skeleton

---
+++ app.js
@@ -0,0 +1,22 @@
+var app = angular.module('ngToDo', []);
+
+app.controller('myNotesCtrl', ['$scope', '$http', function($scope, $http) {
+	$scope.notes = [];
+	
+
+
+
+	$scope.addNotes = function(){
+		
+	}
+
+	$scope.deleteNotes = function(){
+		
+
+	}
+
+  $scope.editNotes = function(){
+    
+  }
+
+}]);
