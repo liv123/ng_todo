343816894dfbe056e55b1bcd66625a4123d13421
liv123 <olivia.e@gmail.com>
2015-10-18 23:20:50 -0700

    added hiding but now deleting wont work until I make an object for notes

--- app.js
+++ app.js
@@ -2,18 +2,26 @@ var app = angular.module('ngToDo', []);
 
 app.controller('myNotesCtrl', ['$scope', '$http', function($scope, $http) {
 	$scope.notes = [];
+	$scope.newNote  = [];
 	
 
 
 
 	$scope.addNote = function(newNote){
 		$scope.notes.push(newNote);
+		$scope.newNote = [];
+
 		
 	}
 
-	$scope.deleteNote = function(){
-		
+	$scope.deleteNote = function(note){
+		$scope.notes.splice(note, 1);
 
+
+	}
+
+	$scope.markComplete = function(){
+		if (checkedOff = true){}
 	}
 
 }]);
